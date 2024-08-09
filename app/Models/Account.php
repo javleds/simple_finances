8d3e7f828c380ce5de3b39c1dbd1a51e8c610f0e
Javier Ledezma <juls0593@gmail.com>
2024-08-09 00:04:54 -0600

    Defining casts

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'user_id' => 'integer',
+            'balance' => 'float',
+        ];
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'user_id' => 'integer',
+            'account_id' => 'integer',
+            'type' => TransactionType::class,
+            'amount' => 'float',
+            'scheduled_at' => 'immutable_datetime',
+        ];
+    }
 }
