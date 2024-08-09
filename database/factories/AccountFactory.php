b4336de29e5c24f93689db6babbf2d33bad1d472
Javier Ledezma <juls0593@gmail.com>
2024-08-08 23:53:32 -0600

    Create account model

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->randomElement([null, $this->faker->text()]),
+            'color' => $this->faker->hexColor(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
