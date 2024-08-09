0e405ea375f33c20868e94edc768eb492b215b6e
Javier Ledezma <juls0593@gmail.com>
2024-08-09 14:23:47 -0600

    Creating MSI outcomes

--- app/Filament/Resources/AccountResource/RelationManagers/TransactionsRelationManager.php
+++ app/Filament/Resources/AccountResource/RelationManagers/TransactionsRelationManager.php
@@ -5,11 +5,15 @@ namespace App\Filament\Resources\AccountResource\RelationManagers;
 use App\Enums\TransactionType;
 use App\Events\BulkTransactionSaved;
 use App\Events\TransactionSaved;
+use App\Models\Account;
 use App\Models\Transaction;
 use Carbon\Carbon;
+use Closure;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Colors\Color;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -126,6 +130,152 @@ class TransactionsRelationManager extends RelationManager
                     })
             ])
             ->headerActions([
+                Tables\Actions\Action::make('add_differed_transaction')
+                    ->label('Crear Egreso Diferido')
+                    ->color(Color::Blue)
+                    ->form([
+                        Forms\Components\Group::make()
+                            ->columns()
+                            ->schema([
+                                Forms\Components\TextInput::make('concept')
+                                    ->label('Concepto')
+                                    ->required()
+                                    ->maxLength(255),
+                                Forms\Components\TextInput::make('amount')
+                                    ->label('Cantidad')
+                                    ->prefix('$')
+                                    ->required()
+                                    ->numeric(),
+                                Forms\Components\DatePicker::make('scheduled_at')
+                                    ->label('Fecha')
+                                    ->prefixIcon('heroicon-o-calendar')
+                                    ->default(Carbon::now())
+                                    ->native(false)
+                                    ->closeOnDateSelection()
+                                    ->required(),
+                                Forms\Components\TextInput::make('payments')
+                                    ->label('No. pagos')
+                                    ->numeric()
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(function (Forms\Set $set, Forms\Get $get) {
+                                        /** @var Account $account */
+                                        $account = $this->getOwnerRecord();
+
+                                        $amount = floatval($get('amount'));
+                                        $payments = intval($get('payments'));
+
+                                        if ($payments <= 0) {
+                                            return;
+                                        }
+
+                                        $paymentSchema = [];
+                                        $paymentAmount = round($amount / $payments, 2);
+
+                                        $date = Carbon::now();
+                                        $cutoffDay = $account->cutoff_day;
+
+                                        $date = $date->day < $cutoffDay
+                                            ? $date->setDay($cutoffDay)->clone()
+                                            : $date->setDay($cutoffDay)->addMonth()->clone();
+
+                                        $paymentSchemaTotal = 0.0;
+                                        foreach (range(1, $payments) as $payment) {
+                                            $paymentSchema[] = [
+                                                'amount' => $paymentAmount,
+                                                'scheduled_at' => $date->clone(),
+                                            ];
+
+                                            $paymentSchemaTotal += $paymentAmount;
+
+                                            $date->addMonth();
+                                        }
+
+                                        $difference = abs($amount - $paymentSchemaTotal);
+
+                                        if ($paymentSchemaTotal > $amount) {
+                                            $value = $paymentSchema[count($paymentSchema) - 1]['amount'];
+                                            $paymentSchema[count($paymentSchema) - 1]['amount'] = round($value - $difference, 2);
+                                        }
+
+                                        if ($paymentSchemaTotal < $amount) {
+                                            $value = $paymentSchema[count($paymentSchema) - 1]['amount'];
+                                            $paymentSchema[count($paymentSchema) - 1]['amount'] = round($value + $difference, 2);
+                                        }
+
+                                        $set('transactions', $paymentSchema);
+                                    })
+                                    ->minValue(1)
+                                    ->required(),
+                                Forms\Components\Repeater::make('transactions')
+                                    ->columnSpanFull()
+                                    ->label('Esquema de pagos')
+                                    ->minItems(fn (Forms\Get $get) => $get('payments'))
+                                    ->maxItems(fn (Forms\Get $get) => $get('payments'))
+                                    ->columns()
+                                    ->schema([
+                                        Forms\Components\TextInput::make('amount')
+                                            ->label('Cantidad')
+                                            ->prefix('$')
+                                            ->required()
+                                            ->numeric(),
+                                        Forms\Components\DatePicker::make('scheduled_at')
+                                            ->label('Fecha')
+                                            ->prefixIcon('heroicon-o-calendar')
+                                            ->default(Carbon::now())
+                                            ->native(false)
+                                            ->closeOnDateSelection()
+                                            ->required(),
+                                    ])
+                                    ->rules([
+                                        fn (Forms\Get $get): Closure => function (string $attribute, $value, Closure $fail) use ($get) {
+                                            $amount = floatval($get('amount'));
+                                            $transactionsTotal = collect($get('transactions'))
+                                                ->reduce(fn (float $total, array $row) => $total += floatval($row['amount']), 0.0);
+
+                                            if ($amount !== $transactionsTotal) {
+                                                $fail(
+                                                    sprintf(
+                                                        'La suma del esquema de pagos ($ %s) debe ser igual a la cantidad del egreso ($ %s)',
+                                                        number_format($transactionsTotal, 2),
+                                                        number_format($amount, 2),
+                                                    )
+                                                );
+                                            }
+                                        }
+                                    ])
+                            ])
+                    ])
+                    ->action(function (array $data, Component $livewire) {
+                        /** @var Account $account */
+                        $account = $this->getOwnerRecord();
+
+                        $concept = $data['concept'];
+                        $amount = floatval($data['amount']);
+                        $date = $data['scheduled_at'];
+                        $payments = intval($data['payments']);
+                        $transactionsToCreate = $data['transactions'];
+
+                        $transactions = collect();
+                        foreach ($transactionsToCreate as $i => $transaction) {
+                            $transactions->add(
+                                $account->transactions()->create([
+                                    'concept' => sprintf('%s - %s de %s (Total $ %s)', $concept, $i + 1, $payments, number_format($amount, 2)),
+                                    'amount' => $transaction['amount'],
+                                    'scheduled_at' => $transaction['scheduled_at'],
+                                    'type' => TransactionType::Outcome,
+                                ])
+                            );
+                        }
+
+                        event(new BulkTransactionSaved($transactions));
+
+                        Notification::make('defer_outcome_done')
+                            ->success()
+                            ->title('Transaccion creada')
+                            ->send();
+
+                        $livewire->dispatch('refreshAccount');
+                    }),
                 Tables\Actions\CreateAction::make()
                     ->createAnother(false)
                     ->after(function (Transaction $record, Component $livewire) {
