17e6e33246e40b005ac31d510dd67142d3ba3314
Javier Ledezma <juls0593@gmail.com>
2024-08-10 14:27:41 -0600

    Change default theme colors

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,12 @@ class AdminPanelProvider extends PanelProvider
             ->emailVerification()
             ->darkMode(isForced: true)
             ->colors([
-                'primary' => Color::Green,
+                'primary' => Color::Teal,
+                'gray' => Color::Zinc,
+                'danger' => Color::Purple,
+                'info' => Color::Blue,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
