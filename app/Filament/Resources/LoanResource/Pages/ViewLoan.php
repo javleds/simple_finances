8c9265f9c427dc6221cec6f4fe8dab8fff7633f8
Javier Ledezma <juls0593@gmail.com>
2024-08-21 01:07:31 -0600

    Add refresh method to loan resource

--- app/Filament/Resources/LoanResource/Pages/ViewLoan.php
+++ app/Filament/Resources/LoanResource/Pages/ViewLoan.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\LoanResource\Pages;
 use App\Filament\Resources\LoanResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Livewire\Attributes\On;
 
 class ViewLoan extends ViewRecord
 {
     protected static string $resource = LoanResource::class;
+
+    #[On('refreshLoan')]
+    public function refresh(): void
+    {
+    }
 }
