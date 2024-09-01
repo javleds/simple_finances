9f678fc9a52a378340dc7a31b8454bb88ac11a6d
Javier Ledezma <juls0593@gmail.com>
2024-08-31 22:16:27 -0600

    Adding balance on notification

--- resources/views/mail/shared/transactions/changed.blade.php
+++ resources/views/mail/shared/transactions/changed.blade.php
@@ -8,6 +8,9 @@
 {{ $transaction->type->getLabel() }}: {{ $transaction->concept }} con un monto de {{  as_money($transaction->amount) }} el {{ $transaction->scheduled_at->translatedFormat('M d, Y') }}.
 </x-mail::panel>
 
+<x-mail::panel>
+Balance después del movimiento: `{{ $transaction->account->balance }}`.
+</x-mail::panel>
 
 <x-mail::button :url="\App\Filament\Resources\AccountResource\Pages\ViewAccount::getUrl([$transaction->account_id])">
 Ir a la cuenta
