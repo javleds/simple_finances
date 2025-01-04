5b09c4bc387c08690a4fe093af4add4404d773d9
Javier Ledezma <juls0593@gmail.com>
2025-01-04 14:33:58 -0600

    Create a command to update the subscription data

---
+++ app/Console/Commands/UpdateSubscriptionData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\PaymentStatus;
+use App\Models\Subscription;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\progress;
+
+class UpdateSubscriptionData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-subscription-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Read the subscription payments to generate the previos and next payment dates.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        progress('Update subscriptions', Subscription::withoutGlobalScopes()->get(), function (Subscription $subscription) {
+            $subscription->withoutGlobalScopes()->update([
+                'previous_payment_date' => $subscription->payments()->where('status', PaymentStatus::Paid)->first()?->scheduled_at,
+                'next_payment_date' => $subscription->payments()->where('status', PaymentStatus::Pending)->first()?->scheduled_at,
+            ]);
+        });
+
+        return self::SUCCESS;
+    }
+}
