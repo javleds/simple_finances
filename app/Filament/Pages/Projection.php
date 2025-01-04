ef1c34a8f31b033ff9f31aa06ca57c8586aa4606
Javier Ledezma <juls0593@gmail.com>
2025-01-04 00:50:31 -0600

    Remove the projection page from navigation

--- app/Filament/Pages/Projection.php
+++ app/Filament/Pages/Projection.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Collection;
 class Projection extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static bool $shouldRegisterNavigation = false;
     protected static string $view = 'filament.resources.subscription-resource.pages.projection';
 
     public Collection $subscriptions;
