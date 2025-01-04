58c1f2ff143276ddbabb91b72b7a8de6418f8b8f
Javier Ledezma <juls0593@gmail.com>
2025-01-04 14:24:19 -0600

    Allow payment takes the payment schema data

--- app/Filament/Actions/CreateSubscriptionPaymentAction.php
+++ app/Filament/Actions/CreateSubscriptionPaymentAction.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Actions;
 
 use App\Enums\Action as UserAction;
+use App\Enums\PaymentStatus;
 use App\Enums\TransactionType;
 use App\Events\TransactionSaved;
 use App\Models\Account;
@@ -24,6 +25,7 @@ class CreateSubscriptionPaymentAction extends Action
         parent::setUp();
 
         $this
+            ->hidden(fn (Subscription $record) => $record->payments()->where('status', PaymentStatus::Pending)->count() === 0)
             ->name('add_payment')
             ->label('Pagar')
             ->color(Color::Emerald)
@@ -37,34 +39,41 @@ class CreateSubscriptionPaymentAction extends Action
                             ->options(fn () => Account::all()->pluck('transfer_balance_label', 'id'))
                             ->default(fn (Subscription $record) => $record->feed_account_id)
                             ->searchable()
-                            ->columnSpanFull()
-                            ->required(),
+                            ->columnSpanFull(),
                         TextInput::make('amount')
                             ->label('Cantidad')
                             ->prefix('$')
                             ->default(fn (Subscription $record) => $record->amount)
-                            ->numeric()
-                            ->required(),
+                            ->numeric(),
                         DatePicker::make('scheduled_at')
                             ->label('Fecha')
                             ->prefixIcon('heroicon-o-calendar')
                             ->native(false)
                             ->closeOnDateSelection()
                             ->default(Carbon::now())
-                            ->maxDate(Carbon::now())
-                            ->required(),
+                            ->maxDate(Carbon::now()),
                     ])
             ])
             ->action(function (array $data, Subscription $record) {
-                $transaction = Transaction::create([
-                    'amount' => $data['amount'],
-                    'type' => TransactionType::Outcome,
-                    'scheduled_at' => $data['scheduled_at'],
-                    'concept' => sprintf('Pago de subscripción %s.', $record->name),
-                    'account_id' => $data['feed_account_id'],
-                ]);
+                $subscription = Subscription::find($record->id);
+                $payment = $subscription->payments()->where('status', PaymentStatus::Pending)->first();
+                $payment->update(['status' => PaymentStatus::Paid]);
 
-                event(new TransactionSaved($transaction, UserAction::Created));
+                $subscription->next_payment_date = $subscription->payments()->where('status', PaymentStatus::Pending)->first()?->scheduled_at;
+                $subscription->previous_payment_date = $subscription->payments()->where('status', PaymentStatus::Paid)->first()?->scheduled_at;
+                $subscription->save();
+
+                if ($data['feed_account_id'] !== null && $data['feed_account_id'] !== '') {
+                    $transaction = Transaction::create([
+                        'amount' => $data['amount'],
+                        'type' => TransactionType::Outcome,
+                        'scheduled_at' => $data['scheduled_at'],
+                        'concept' => sprintf('Pago de subscripción %s.', $record->name),
+                        'account_id' => $data['feed_account_id'],
+                    ]);
+
+                    event(new TransactionSaved($transaction, UserAction::Created));
+                }
 
                 Notification::make('saved')
                     ->success()
