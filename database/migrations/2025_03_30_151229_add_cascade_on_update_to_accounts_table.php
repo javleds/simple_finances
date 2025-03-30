d9104c5aebd5a7e572d81e8df14335418d500f7e
Javier Ledezma <juls0593@gmail.com>
2025-03-30 15:20:20 -0600

    Allow to delete accounts in cascade

---
+++ database/migrations/2025_03_30_151229_add_cascade_on_update_to_accounts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropForeign('idx_feed_account');
+            $table->foreign('feed_account_id', 'idx_feed_account')->references('id')->on('accounts')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            //
+        });
+    }
+};
