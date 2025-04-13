8b42c71e18e3de1b1949f6cc5e9595f48529b75c
Javier Ledezma <juls0593@gmail.com>
2025-04-12 21:08:42 -0600

    Fix the daily updater

--- app/Services/SubscriptionUpdater.php
+++ app/Services/SubscriptionUpdater.php
@@ -9,11 +9,14 @@ class SubscriptionUpdater
 {
     public function handle(): void
     {
-        $subscriptions = Subscription::withoutGlobalScopes()->where('next_cutoff_date', '<', Carbon::now());
+        $subscriptions = Subscription::withoutGlobalScopes()
+            ->whereNull('next_payment_date')
+            ->orWhere('next_payment_date', '<', Carbon::now());
 
         /** @var Subscription $subscription */
         foreach ($subscriptions as $subscription) {
-            $subscription->previous_payment_date = $subscription->next_payment_date->clone();
+            $previousPayment = $subscription->next_payment_date === null ? null : $subscription->next_payment_date->clone();
+            $subscription->previous_payment_date = $previousPayment;
             $subscription->next_payment_date = $subscription->getNextPaymentDate();
 
             $subscription->saveQuietly();
