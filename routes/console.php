9f27061e51ba75e7683e4aa537a656195b67383a
Javier Ledezma <juls0593@gmail.com>
2025-04-12 20:43:05 -0600

    Fix the subscription updater to save quitely

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Services\AutomatedAccountUpdater;
+use App\Services\Subscriptions\DailyUpdater;
 use App\Services\SubscriptionUpdater;
 use App\Services\WeeklySummaryProcessor;
 use Illuminate\Support\Facades\Schedule;
@@ -12,6 +13,11 @@ Schedule::call(function() {
 Schedule::call(function() {
     app(SubscriptionUpdater::class)->handle();
 })->dailyAt('00:30');
+
+// Uncomment the next block if SubscriptionUpdater code is not working
+//Schedule::call(function() {
+//    app(DailyUpdater::class)->handle();
+//})->dailyAt('00:30');
 
 Schedule::call(function() {
     app(WeeklySummaryProcessor::class)->handle();
