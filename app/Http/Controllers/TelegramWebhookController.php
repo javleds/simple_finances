8b17a0982d08c37ccfe59631467ca1ba05e502ac
Javier Ledezma <juls0593@gmail.com>
2025-09-23 01:00:16 -0600

    Update the respons eo fthe webhook

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\TelegramServiceInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
@@ -13,16 +14,18 @@ class TelegramWebhookController extends Controller
     {
     }
 
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request): JsonResponse
     {
         try {
+            logger()->debug('Webhook de Telegram recibido', $request->all());
+
             $chatId = data_get($request->all(), 'message.chat.id');
 
             $messageText = data_get($request->all(), 'message.text');
 
             if (empty($chatId)) {
                 Log::warning('Webhook de Telegram recibido sin chat ID', $request->all());
-                return response('', Response::HTTP_OK);
+                return response()->json(['message' => 'No chat ID!'], Response::HTTP_UNPROCESSABLE_ENTITY);
             }
 
             if (empty($messageText)) {
@@ -33,7 +36,7 @@ class TelegramWebhookController extends Controller
 
             $this->telegramService->sendMessage((string) $chatId, $responseMessage);
 
-            return response('', Response::HTTP_OK);
+            return response()->json([], Response::HTTP_OK);
 
         } catch (\Exception $e) {
             Log::error('Error procesando webhook de Telegram', [
@@ -41,7 +44,7 @@ class TelegramWebhookController extends Controller
                 'webhook_data' => $request->all()
             ]);
 
-            return response('', Response::HTTP_OK);
+            return response()->json(['message' => 'Error procesando webhook de Telegram'], Response::HTTP_SERVICE_UNAVAILABLE);
         }
     }
 }
