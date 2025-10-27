8714af0e3458db19a9d237492173852abfe51bf9
Javier Ledezma <juls0593@gmail.com>
2025-10-26 21:56:30 -0600

    Update dependencies to php 8.4

--- resources/views/tables/columns/progress-column.blade.php
+++ resources/views/tables/columns/progress-column.blade.php
@@ -1,6 +1,7 @@
-<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 relative overflow-hidden">
-    <div class="bg-primary-500 h-2.5 text-center" style="width:{{ $getState() }}%">&nbsp;</div>
-    <span class="absolute w-full h-full top-0 left-0 text-center">
-        {{ $getState() }}%
-    </span>
+<div class="w-full bg-gray-200 rounded-full h-4 dark:bg-gray-700 relative overflow-hidden">
+    <div class="bg-primary-500 h-4 text-center" style="width:{{ $getState() }}%">&nbsp;
+        <span class="absolute w-full h-full top-0 left-0 flex items-center justify-center text-white text-sm font-medium">
+            {{ $getState() }}%
+        </span>
+    </div>
 </div>
