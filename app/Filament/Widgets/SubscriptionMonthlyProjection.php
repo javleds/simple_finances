df2c99092d12412c22b1e3a9c2d47d7b7a648aea
Javier Ledezma <juls0593@gmail.com>
2025-12-07 18:54:23 -0600

    Add yearly sub statu

--- app/Filament/Widgets/SubscriptionMonthlyProjection.php
+++ app/Filament/Widgets/SubscriptionMonthlyProjection.php
@@ -2,8 +2,6 @@
 
 namespace App\Filament\Widgets;
 
-use App\Enums\Frequency;
-use App\Models\Account;
 use App\Models\Subscription;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -14,26 +12,51 @@ class SubscriptionMonthlyProjection extends BaseWidget
     {
         $subscriptions = Subscription::whereNull('finished_at')->get();
 
-        $amount = 0.0;
-        foreach ($subscriptions as $subscription) {
-            if ($subscription->frequency_type === Frequency::Month) {
-                $amount += $subscription->amount;
-
-                continue;
-            }
-
-            if ($subscription->frequency_type === Frequency::Year) {
-                $amount += ($subscription->amount / 12);
-
-                continue;
-            }
-
-            $amount += round(($subscription->pricing / ($subscription->periodicity_every * 100 / 30.4)), 2);
-        }
+        $monthlyAmount = $subscriptions->sum(
+            fn (Subscription $subscription): float => $this->getMonthlyProjectionAmount($subscription)
+        );
+        $yearlyAmount = $subscriptions->sum(
+            fn (Subscription $subscription): float => $this->getYearlyProjectionAmount($subscription)
+        );
 
         return [
-            Stat::make('Ahorro mensual recomendado', as_money($amount)),
-            Stat::make('Ahorro quincenal recomendado', as_money($amount / 2)),
+            Stat::make('Ahorro mensual recomendado', as_money($monthlyAmount)),
+            Stat::make('Ahorro quincenal recomendado', as_money($monthlyAmount / 2)),
+            Stat::make('Gasto anual en suscripciones', as_money($yearlyAmount)),
         ];
+    }
+
+    private function getMonthlyProjectionAmount(Subscription $subscription): float
+    {
+        if ($subscription->isMonthly()) {
+            return $subscription->amount;
+        }
+
+        if ($subscription->isYearly()) {
+            return $subscription->amount / 12;
+        }
+
+        if ($subscription->isDaily()) {
+            return ($subscription->amount / $subscription->frequency_unit) * 30.4;
+        }
+
+        return $subscription->amount;
+    }
+
+    private function getYearlyProjectionAmount(Subscription $subscription): float
+    {
+        if ($subscription->isMonthly()) {
+            return $subscription->amount * 12;
+        }
+
+        if ($subscription->isYearly()) {
+            return $subscription->amount;
+        }
+
+        if ($subscription->isDaily()) {
+            return ($subscription->amount / $subscription->frequency_unit) * 365;
+        }
+
+        return $subscription->amount;
     }
 }
