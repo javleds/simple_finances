57c8d975319c42719646377011d11de00cee234f
Javier Ledezma <juls0593@gmail.com>
2026-06-10 11:49:19 -0600

    Add virtual in graph endpoint

--- app/Services/Dashboard/BuildDashboardGraph.php
+++ app/Services/Dashboard/BuildDashboardGraph.php
@@ -11,12 +11,13 @@ class BuildDashboardGraph
     {
         return Account::query()
             ->orderBy('name')
-            ->get(['id', 'name', 'balance', 'color'])
+            ->get(['id', 'name', 'balance', 'color', 'virtual'])
             ->map(fn (Account $account): array => [
                 'account_id' => $account->id,
                 'account_name' => $account->name,
                 'balance' => $account->balance,
                 'color' => $account->color,
+                'is_virtual' => $account->virtual,
             ]);
     }
 }
